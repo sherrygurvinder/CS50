0e7942fd3672257cb120891c7fde5ba14736c24e
Gurvinder <gurvinder.singh@chd.trantorinc.com>
2017-01-18 17:07:26 +0530

    problem set 1 mario2.c

---
+++ week1/mario2.c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+
+int get_int()
+{
+
+	int n;
+	scanf("%d",&n);
+
+while(n<=0&&n>=24)
+{
+	printf("RETRY:");
+	scanf("%d",&n);
+}
+
+return n;
+}
+void main()
+{
+
+	int n,i,j,k,count=0;
+	printf("ENTER THE HEIGHT: ");
+	n=get_int();
+	count=n;
+	for(i=n;i>0;i--)
+	{	       	
+		for(j=0;j<=n*2;j++)
+		{
+		if(j==n)
+		{
+			printf(" ");
+		}
+
+		if(j<n)
+		{
+		
+			if(j<i-1)
+		{
+				printf(" ");
+		}
+			else
+		{
+				printf("#");
+		}
+
+		}
+			if(j>n)
+			{
+				if(j<=count+1)
+				{
+					printf("#");
+				}
+		else
+		{
+				printf(" ");
+		}
+
+		}
+	
+	}
+		count++;
+
+	
+	printf("\n");
+}
+}
