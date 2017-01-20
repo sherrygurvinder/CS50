8a92ee64529381570d5cad080d9d0227879fecca
Gurvinder <gurvinder.singh@chd.trantorinc.com>
2017-01-20 16:29:44 +0530

    initials.c

--- week2/initials.c
+++ week2/initials.c
@@ -1,31 +1,29 @@
 #include<stdio.h>
+#include<string.h>
 void main()
 {
-
-char str[50];
-printf("enter the string \n");
-fgets(str,50,stdin);
-int i=0;
-
-while(str[i]!='\0')
-{
-if(str[i]!=' ')
-{
-if(i==0||str[i-1]==' ')
-{
-if(str[i]>=97&&str[i]<=122)
-{
-printf("%c",str[i]-32);
-}
-else
-{
-printf("%c",str[i]);
-}
-}
-}
-i++;
-}
-printf("\n");
+	char str[50];
+	gets(str);
+	int i=0;
+	while(str[i]!='\0')
+	{
+		if(str[i]!=' ')
+		{
+			if(i==0||str[i-1]==' ')
+			{
+				if(str[i]>=97&&str[i]<=122)
+				{
+					printf("%c",str[i]-32);
+				}
+				else
+				{
+					printf("%c",str[i]);
+				}
+			}
+		}
+	i++;
+	}
+	printf("\n");
 
 }
 
